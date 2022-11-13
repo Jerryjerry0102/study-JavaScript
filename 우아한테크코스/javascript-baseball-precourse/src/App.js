35affe6fffe001ef24fc1e179fdd2336f425e188
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-13 20:38:46 +0900

    feat(baseball): 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- 우아한테크코스/javascript-baseball-precourse/src/App.js
+++ 우아한테크코스/javascript-baseball-precourse/src/App.js
@@ -1,6 +1,7 @@
 const MissionUtils = require("@woowacourse/mission-utils");
 const Judgement = require("./domain/Judgement");
 const NumberGenerator = require("./domain/NumberGenerator");
+const Referee = require("./domain/Referee");
 
 class App {
   play() {
@@ -8,11 +9,15 @@ class App {
     // const numbers = numberGenerator.createRandomNumbers();
     // console.log(numbers);
 
-    const judgement = new Judgement();
+    // const judgement = new Judgement();
     // const count = judgement.correctCount([7, 8, 9], [1, 2, 3]);
     // console.log(count);
-    const place = judgement.hasPlace([7, 8, 9], 0, 7);
-    console.log(place);
+    // const place = judgement.hasPlace([7, 8, 9], 0, 7);
+    // console.log(place);
+
+    const referee = new Referee();
+    const result = referee.compare([3, 1, 2], [1, 2, 3]);
+    console.log(result);
   }
 }
 
--- 우아한테크코스/javascript-baseball-precourse/src/domain/Referee.js
+++ 우아한테크코스/javascript-baseball-precourse/src/domain/Referee.js
@@ -1,6 +1,21 @@
+const Judgement = require("./Judgement");
+
 class Referee {
   compare = (computer, player) => {
-    return "3 스트라이크";
+    // 몇 개의 숫자가 같은지 알아낸 뒤
+    // 스트라이크 개수를 구해 뺀다.
+    // 남은 수는 볼의 개수이다.
+    const judgement = new Judgement();
+    let correctCount = judgement.correctCount(computer, player);
+
+    let strike = 0;
+    for (let index = 0; index < computer.length; index++) {
+      if (judgement.hasPlace(computer, index, player[index])) {
+        strike++;
+      }
+    }
+    let ball = correctCount - strike;
+    return ball + " 볼" + strike + " 스트라이크";
   };
 }
 
