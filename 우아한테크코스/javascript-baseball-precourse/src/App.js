a49a9e611c32d0b0e1bfeeab8a04dba1f4b09679
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-13 19:03:12 +0900

    feat(baseball): 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- 우아한테크코스/javascript-baseball-precourse/src/App.js
+++ 우아한테크코스/javascript-baseball-precourse/src/App.js
@@ -1,7 +1,12 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const NumberGenerator = require("./domain/NumberGenerator");
 
 class App {
-  play() {}
+  play() {
+    const numberGenerator = new NumberGenerator();
+    const numbers = numberGenerator.createRandomNumbers();
+    console.log(numbers);
+  }
 }
 
 module.exports = App;
--- 우아한테크코스/javascript-baseball-precourse/src/domain/NumberGenerator.js
+++ 우아한테크코스/javascript-baseball-precourse/src/domain/NumberGenerator.js
@@ -1,6 +1,29 @@
 class NumberGenerator {
   createRandomNumbers = () => {
-    return null;
+    /*
+    let numbers = new Array();
+    for (let i = 0; i < 3; i++) {
+      let number = Math.floor(Math.random() * 9) + 1;
+      numbers.push(number);
+    }
+    return numbers;
+    */
+    /* 
+    3개의 숫자가 담길 때까지
+    만약 이미 존재하는 숫자라면 담지 않는다.
+    만약 존재하지 않는 숫자라면 담는다.
+    -> 이런 경우에는 for문으로는 해결 x, while 문으로 사용해야 함
+    */
+    let numbers = new Array();
+    while (numbers.length < 3) {
+      let number = Math.floor(Math.random() * 9) + 1;
+      if (numbers.includes(number)) {
+        continue;
+      } else {
+        numbers.push(number);
+      }
+    }
+    return numbers;
   };
 }
 
