aedd69996fdd9a6064a91b9ebeab0633f5a39f4a
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-13 19:43:49 +0900

    feat(baseball): 몇 개의 숫자가 같은지 알 수 있는 기능 구현

--- 우아한테크코스/javascript-baseball-precourse/src/App.js
+++ 우아한테크코스/javascript-baseball-precourse/src/App.js
@@ -1,11 +1,16 @@
 const MissionUtils = require("@woowacourse/mission-utils");
+const Judgement = require("./domain/Judgement");
 const NumberGenerator = require("./domain/NumberGenerator");
 
 class App {
   play() {
-    const numberGenerator = new NumberGenerator();
-    const numbers = numberGenerator.createRandomNumbers();
-    console.log(numbers);
+    // const numberGenerator = new NumberGenerator();
+    // const numbers = numberGenerator.createRandomNumbers();
+    // console.log(numbers);
+
+    const judgement = new Judgement();
+    const count = judgement.correctCount([7, 8, 9], [1, 2, 3]);
+    console.log(count);
   }
 }
 
--- 우아한테크코스/javascript-baseball-precourse/src/domain/Judgement.js
+++ 우아한테크코스/javascript-baseball-precourse/src/domain/Judgement.js
@@ -1,9 +1,16 @@
 class Judgement {
   correctCount = (computer, player) => {
-    return 0;
+    let result = 0;
+    for (let i = 0; i < player.length; i++) {
+      let playerNumber = player[i];
+      if (computer.includes(playerNumber)) {
+        result++; // result = result + 1;
+      }
+    }
+    return result;
   };
 
-  hasPlace = (place, number) => {
+  hasPlace = (computer, place, number) => {
     return false;
   };
 }
