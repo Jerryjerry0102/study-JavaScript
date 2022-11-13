4f1a1ab8db2a35f8e29f4a8e1477518cef11c5c8
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-13 17:36:36 +0900

    java강의로 javascript Class 공부

--- 우아한테크코스/javascript-baseball-precourse/src/domain/Calculator.js
+++ 우아한테크코스/javascript-baseball-precourse/src/domain/Calculator.js
@@ -1,7 +1,7 @@
 class Calculator {
   /* public, private 구분위해
-  result = 0;
-  #result = 0;
+  result = 0;   -> public
+  #result = 0;  -> private
   */
   result = 0; // 인스턴스 변수
   /* 
@@ -10,7 +10,21 @@ class Calculator {
   이렇게 되면 object와 관련 없이 Class 자체에 연결되어 있는 것
   */
   static shareResult = 0; // 클래스 변수
-  constructor() {}
+  /* 상수, 클래스 변수이면서 변하지 않는 값
+  공유는 하고 싶은데 값은 변하지 않게 불변으로 막아주고 싶을 때
+  java에서는 public static final을 사용하는데 javascript에서는 모르겠음
+  const인 거 같기는 한데 static const 이렇게 사용이 안 됨.
+  인터넷 찾아보니까 Class 안에서 상수는
+  static get shareBirthDay(){ return 0102 } 이렇게 씀
+  */
+  static get shareBirthDay() {
+    return 102;
+  }
+
+  constructor() {
+    // this.result = 0;
+  }
+  // 메소드
   add = (number1, number2) => {
     this.result = number1 + number2;
     // result를 add뿐만 아니라 밖에서도 쓸 수 있게 하기 위해서 this.result 해 줌
