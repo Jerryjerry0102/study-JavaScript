d6e5736c6b72e61c76e61ec6d5d546ae8b432889
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-11-30 19:59:27 +0900

    모던 자바스크립트 Deep Dive: 17 read

---
+++ 모던자바스크립트/17/17-01.js
@@ -0,0 +1,13 @@
+// 빈 객체의 생성
+const person = new Object();
+
+console.log(person); // {}
+
+// 프로퍼티 추가
+person.name = "Lee";
+person.sayHello = function () {
+  console.log("Hi! My name is " + this.name);
+};
+
+console.log(person); // {name: "Lee", sayHello: ƒ}
+person.sayHello(); // Hi! My name is Lee
