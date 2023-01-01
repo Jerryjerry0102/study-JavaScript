7d62dbb551a8c6bba65f94ebc110bb0d674ac2e6
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-01-01 23:22:27 +0900

    모던 자바스크립트 Deep Dive: 46 read

---
+++ ModernJavaScript/46/46-14.js
@@ -0,0 +1,13 @@
+// const fetch = require('node-fetch');
+import fetch from 'node-fetch';
+
+async function fetchTodo() {
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+  const response = await fetch(url);
+  const todo = await response.json();
+  console.log(todo);
+  // {userId: 1, id: 1, title: 'delectus aut autem', completed: false}
+}
+
+fetchTodo();
--- ModernJavaScript/docs/README.md
+++ ModernJavaScript/docs/README.md
@@ -52,7 +52,7 @@
 - [x] 43장 Ajax
 - [x] 44장 REST API
 - [x] 45장 프로미스
-- [ ] 46장 제너레이터와 async/await
+- [x] 46장 제너레이터와 async/await
 - [ ] 47장 에러 처리
 - [ ] 48장 모듈
 - [ ] 49장 Babel과 Webpack을 이용한 ES6+/ES.NEXT 개발 환경 구축
