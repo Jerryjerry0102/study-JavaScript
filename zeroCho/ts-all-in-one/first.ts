fe86b5f8893f931f7721effc0f02f333bfdd49d9
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-02-15 16:43:16 +0900

    타입 추론을 적극 활용하자

--- zeroCho/ts-all-in-one/first.ts
+++ zeroCho/ts-all-in-one/first.ts
@@ -1,43 +1,39 @@
-// 타입을 붙여주는 행위를 타입핑한다라고 함
-// 타입스크립트는 에러메시지가 상세하게 나와있다. 잘 읽어보자
+// 타입스크립트는 타입을 추론하는 기능이 있기 때문에
+// 타입이 제대로 추론되면 타입핑을 굳이 할 필요가 없다.
+// 타입스크립트가 추론을 잘못하거나 any라는 타입으로 추론할 경우 직접 타입핑
+
+// 오히려 타입을 쓰는 것이 문제가 될 수도 있다.
+// const a: string = "5";
+// 타입은 최대한 정확하게 하는 것이 중요
+// a는 평생 '5'인데 string이라는 타입으로 부정확하게 만듬
 
 /** 변수 타입핑 */
-const a: string = "hello";
-const b: number = 5;
-const c: boolean = true;
-const d: undefined = undefined;
-const e: null = null;
-// any는 뒤에 아무거나 넣을 수 있다.
-// 타입스크립트의 주 목적은 any를 없애는 것
+const a = "hello";
+const b = 5;
+const c = true;
+const d = undefined;
+const e = null;
+
 const f: any = 123;
-// 타입자리에 아예 고정된 원시값을 넣을 수도 있다.
-// 타입은 최대한 정확한 게 좋은데 const는 바뀔 일이 없기 때문에!
 const g: true = true;
 const h: 5 = 5;
 
 /** 함수 타입핑 */
-function add(x: number, y: number): number {
+// x도 number y도 number이기 때문에 둘을 더하면 당연히 number
+// 이렇게 정확할 경우에는 result를 number로 만들어 준다.
+function add(x: number, y: number) {
   return x + y;
 }
-// 화살표 함수 타입핑1
-const add2: (x: number, y: number) => number = (x, y) => x + y;
-// 화살표 함수 타입핑2
-// 타입을 빼줄 수 있다.
-type Add = (x: number, y: number) => number;
-const add3: Add = (x, y) => x + y;
-// 인터페이스로 함수 타입핑(심화 과정)
-interface Add2 {
-  (x: number, y: number): number;
-}
-const add4: Add2 = (x, y) => x + y;
+const result = add(1, 2);
 
 /** 객체 타입핑 */
-const obj: { lat: number; lon: number } = { lat: 37.5, lon: 127.5 };
+const obj = { lat: 37.5, lon: 127.5 };
 
 /** 배열 타입핑 */
-// 기본
-const arr: string[] = ["123", "456"];
-// 제네릭(심화 과정)
-const arr2: Array<number> = [123, 456];
-// 튜플(길이가 고정된 배열)
+const arr = ["123", "456"];
+const arr2 = [123, 456];
 const arr3: [number, number, string] = [123, 456, "hello"];
+
+// ->
+// 추론이 잘되면 최대한 추론에 맡기자
+// 타입은 최대한 좁게 잡자
