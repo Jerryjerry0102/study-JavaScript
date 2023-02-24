a0d2136d9480a0bed5f8c0190ed92ab59742b928
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-02-25 04:56:30 +0900

    forEach, map, filter 제네릭 분석 및 타입 직접 만들기

--- zeroCho/ts-all-in-one/first.ts
+++ zeroCho/ts-all-in-one/first.ts
@@ -1,41 +1,14 @@
-// 타입스크립트는 타입을 추론하는 기능이 있기 때문에
-// 타입이 제대로 추론되면 타입핑을 굳이 할 필요가 없다.
-// 타입스크립트가 추론을 잘못하거나 any라는 타입으로 추론할 경우 직접 타입핑
-
-// 오히려 타입을 쓰는 것이 문제가 될 수도 있다.
-// const a: string = "5";
-// 타입은 최대한 정확하게 하는 것이 중요
-// a는 평생 '5'인데 string이라는 타입으로 부정확하게 만듬
-
-/** 변수 타입핑 */
-const a = "hello";
-const b = 5;
-const c = true;
-const d = undefined;
-const e = null;
-
-const f: any = 123;
-const g: true = true;
-const h: 5 = 5;
-
-/** 함수 타입핑 */
-// x도 number y도 number이기 때문에 둘을 더하면 당연히 number
-// 이렇게 정확할 경우에는 result를 number로 만들어 준다.
-function add(x: number, y: number) {
-  return x + y;
+interface Arr<T> {
+  forEach(callbackfn: (v: T, i: number, array: T[]) => void): void;
+  map<S>(callbackfn: (v: T, i: number, array: T[]) => S): S[];
+  filter<S extends T>(callback: (v: T, i: number, array: T[]) => v is S): S[];
 }
-const result = add(1, 2);
 
-/** 객체 타입핑 */
-const obj = { lat: 37.5, lon: 127.5 };
+const a: Arr<number> = [1, 2, 3];
+const b = a.filter((v): v is number => v % 2 === 0); // [2] number[]
 
-/** 배열 타입핑 */
-const arr = ["123", "456"];
-const arr2 = [123, 456];
-const arr3: [number, number, string] = [123, 456, "hello"];
+const c: Arr<number | string> = [1, "2", 3, "4", 5];
+const d = c.filter((v): v is string => typeof v === "string"); // ['2', '4'] string[]
 
-// ->
-// 추론이 잘되면 최대한 추론에 맡기자
-// 타입은 최대한 좁게 잡자
-
-// 여기서부터 기본 문법은 노션 정리
+const predicate = (v: string | number): v is number => typeof v === "number";
+const e = c.filter(predicate); // [1, 3, 5] number[]
