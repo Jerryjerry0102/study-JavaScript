e5291eb336c065a15bc3063f73ae4fae7f014b6b
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-02-28 02:52:33 +0900

    4 LOGIN complete

--- NomadCoders/MOMENTUM/app.js
+++ NomadCoders/MOMENTUM/app.js
@@ -1,18 +1,28 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 const handleLoginFormSubmit = (event) => {
   event.preventDefault(); // 기본 동작을 막아준다.
-  username = loginInput.value;
-  console.log(username);
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
 };
 
-const handleLinkClick = (event) => {
-  event.preventDefault();
-  alert("click");
-};
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-loginForm.addEventListener("submit", handleLoginFormSubmit);
-link.addEventListener("click", handleLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", handleLoginFormSubmit);
+} else {
+  paintGreetings(savedUsername);
+}
