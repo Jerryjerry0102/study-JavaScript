3e47fc14de522afb1ba3f8854ec25d2d12c611af
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-06-23 18:52:20 +0900

    URL 라우팅 처리

--- Programmers/2021-Dev-Matching2(p)/App.js
+++ Programmers/2021-Dev-Matching2(p)/App.js
@@ -2,4 +2,22 @@ import ProductDetailPage from "./pages/ProductDetailPage.js";
 import ProductListPage from "./pages/ProductListPage.js";
 import CartPage from "./pages/CartPage.js";
 
-export default function App($app) {}
+export default function App($app) {
+  const productListPage = new ProductListPage($app);
+  const productDetailPage = new ProductDetailPage($app);
+  const cartPage = new CartPage($app);
+
+  this.checkRoute = () => {
+    const { pathname } = window.location;
+    if (pathname === "/") {
+      productListPage.render();
+    } else if (pathname.includes("/products")) {
+      const productId = pathname.split("/")[2];
+      productDetailPage.setState(productId);
+    } else if (pathname === "/cart") {
+      cartPage.render();
+    }
+  };
+
+  this.checkRoute();
+}
--- Programmers/2021-Dev-Matching2(p)/pages/ProductListPage.js
+++ Programmers/2021-Dev-Matching2(p)/pages/ProductListPage.js
@@ -1,9 +1,12 @@
 export default function ProductListPage($app) {
   this.$page = document.createElement("div");
   this.$page.className = "ProductListPage";
-  $app.append(this.$page);
 
   this.$h1 = document.createElement("h1");
   this.$h1.textContent = "상품 목록";
   this.$page.append(this.$h1);
+
+  this.render = () => {
+    $app.append(this.$page);
+  };
 }
