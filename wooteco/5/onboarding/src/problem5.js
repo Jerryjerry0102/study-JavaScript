387cd74858f219d015217223d99448a0914d307a
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-08-04 18:06:38 +0900

    problem5 기능 구현 완료

--- wooteco/5/onboarding/src/problem5.js
+++ wooteco/5/onboarding/src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < moneyUnit.length; i++) {
+    if (money === 0) break;
+    if (money >= moneyUnit[i]) {
+      answer[i] = Math.floor(money / moneyUnit[i]);
+      money = money % moneyUnit[i];
+    }
+  }
   return answer;
 }
 
