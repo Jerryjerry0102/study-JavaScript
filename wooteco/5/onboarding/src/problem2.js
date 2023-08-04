03aa419fdc699e0e3def6bcc8749f9f07bdd20f4
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-08-04 17:15:40 +0900

    problem2 기능 구현 완료

--- wooteco/5/onboarding/src/problem2.js
+++ wooteco/5/onboarding/src/problem2.js
@@ -1,5 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
+  const stack = [cryptogram[0]];
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stack[stack.length - 1] === cryptogram[i]) stack.pop();
+    else stack.push(cryptogram[i]);
+  }
+  const answer = stack.join("");
   return answer;
 }
 
