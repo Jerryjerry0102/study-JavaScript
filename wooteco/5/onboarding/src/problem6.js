ef8748c58c9298bb5ac378a13f053014dce6a455
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-08-04 19:05:35 +0900

    problem6 기능 구현 완료

--- wooteco/5/onboarding/src/problem6.js
+++ wooteco/5/onboarding/src/problem6.js
@@ -1,5 +1,28 @@
 function problem6(forms) {
-  var answer;
+  const set = new Set();
+  function compareNickname(nickname1, nickname2) {
+    for (let i = 0; i < nickname1.length - 1; i++) {
+      for (let j = 0; j < nickname2.length - 1; j++) {
+        if (
+          nickname1[i] === nickname2[j] &&
+          nickname1[i + 1] === nickname2[j + 1]
+        )
+          return true;
+      }
+    }
+    return false;
+  }
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms.length; j++) {
+      if (i === j) break;
+      if (compareNickname(forms[i][1], forms[j][1])) {
+        set.add(forms[i][0]);
+        set.add(forms[j][0]);
+      }
+    }
+  }
+  let answer = [...set];
+  answer.sort();
   return answer;
 }
 
