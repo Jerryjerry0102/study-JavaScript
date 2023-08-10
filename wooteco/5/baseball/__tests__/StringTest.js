13e68362b04f41cb31d9b5aeea0568799c6ba0de
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-08-10 21:23:31 +0900

    Jest 학습

--- wooteco/5/baseball/__tests__/StringTest.js
+++ wooteco/5/baseball/__tests__/StringTest.js
@@ -1,7 +1,7 @@
 describe("문자열 테스트", () => {
   test("split 메서드로 주어진 값을 구분", () => {
     const input = "1,2";
-    const result = input.split(",");
+    const result = input.split(","); // ['1', '2']
 
     expect(result).toContain("2", "1");
     expect(result).toContainEqual("1", "2");
@@ -9,22 +9,25 @@ describe("문자열 테스트", () => {
 
   test("split 메서드로 구분자가 포함되지 않은 경우 값을 그대로 반환", () => {
     const input = "1";
-    const result = input.split(",");
+    const result = input.split(","); // ["1"];
 
     expect(result).toContain("1");
   });
 
   test("substring 메서드로 특정 구간 값을 반환", () => {
     const input = "(1,2)";
-    const result = input.substring(1, 4);
+    const result = input.substring(1, 4); // '1,2'
 
+    expect(result).toBe("1,2");
     expect(result).toEqual("1,2");
+    expect(result).toContain("1,");
   });
 
   test("repeat 메서드로 문자열을 여러번 반복", () => {
     const input = "abc";
-    const result = input.repeat(3);
+    const result = input.repeat(3); // 'abcabcabc'
 
+    expect(result).toBe("abcabcabc");
     expect(result).toEqual("abcabcabc");
   });
 
@@ -32,6 +35,11 @@ describe("문자열 테스트", () => {
     const input = "abc";
     const result = () => input.repeat(-1);
 
+    expect(result).toThrow();
+    expect(result).toThrow(Error);
     expect(result).toThrow(RangeError);
+    expect(result).toThrow("Invalid");
+    expect(result).toThrow(/Invalid count/);
+    expect(result).toThrow(/^Invalid count value$/);
   });
 });
