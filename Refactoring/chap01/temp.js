c6660f750824c756645258c217b26aa293506a90
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 03:33:28 +0900

    1.4 적립 포인트 계산 코드 추출하기: 변수 이름 명확하게 바꾸기(perf -> aPerformance, volumeCredits -> result)

--- Refactoring/chap01/temp.js
+++ Refactoring/chap01/temp.js
@@ -12,7 +12,7 @@ function statement(invoice, plays) {
   }).format;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf); //-> 추출한 함수를 이용해 값을 누적
+    volumeCredits += volumeCreditsFor(perf);
 
     // 청구 내역을 출력한다.
     result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
@@ -53,13 +53,12 @@ function statement(invoice, plays) {
     return plays[aPerformance.playID];
   }
 
-  //-> 새로 추출한 함수
-  function volumeCreditsFor(perf) {
-    let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type)
-      volumeCredits += Math.floor(perf.audience / 5);
-    return volumeCredits;
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
   }
 }
 
