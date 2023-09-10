ee776a28fba257cfe0d0cca9d4a6039d6e5b0edd
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 04:50:10 +0900

    1.4 totalAmount 변수 제거하기: 반복문 쪼개기, 문장 슬라이드하기, 함수 추출하기 & 임시 이름 부여(appleSauce)

--- Refactoring/chap01/temp.js
+++ Refactoring/chap01/temp.js
@@ -2,7 +2,6 @@ import invoices from "./invoices.json" assert { type: "json" };
 import plays from "./plays.json" assert { type: "json" };
 
 function statement(invoice, plays) {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -10,10 +9,11 @@ function statement(invoice, plays) {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
+  let totalAmount = appleSauce(); //-> 함수 추출 & 임시 이름 부여
+
   result += `총액: ${usd(totalAmount)}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`; //-> 변수 인라인
+  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
   function amountFor(aPerformance) {
@@ -66,6 +66,14 @@ function statement(invoice, plays) {
       volumeCredits += volumeCreditsFor(perf);
     }
     return volumeCredits;
+  }
+
+  function appleSauce() {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      totalAmount += amountFor(perf);
+    }
+    return totalAmount;
   }
 }
 
