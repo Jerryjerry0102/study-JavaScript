9699b787dc68dc28256bbe9b74a6c793dcf8b113
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-10 10:15:35 +0900

    1.4 amountFor()를 statement()의 중첩 함수로 만들기

--- Refactoring/chap01/temp.js
+++ Refactoring/chap01/temp.js
@@ -13,7 +13,7 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play); // 추출한 함수를 이용
+    let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
@@ -30,31 +30,30 @@ function statement(invoice, plays) {
   result += `총액: ${format(totalAmount / 100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-}
 
-function amountFor(perf, play) {
-  // 값이 바귀지 않는 변수는 매개변수로 전달
-  let thisAmount = 0;
+  function amountFor(perf, play) {
+    let thisAmount = 0;
 
-  switch (play.type) {
-    case "tragedy":
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy":
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르": ${play.type}`);
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르": ${play.type}`);
+    }
+
+    return thisAmount;
   }
-
-  return thisAmount; // 함수 안에서 값이 바뀌는 변수 반환
 }
 
 console.log(statement(invoices[0], plays));
