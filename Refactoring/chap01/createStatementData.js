9423e50f6fe4d88df3c7a12cd3aaefbdfd3623ed
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 20:29:02 +0900

    1.8 공연료 계산기 만들기: 함수 선언 바꾸기

--- Refactoring/chap01/createStatementData.js
+++ Refactoring/chap01/createStatementData.js
@@ -7,9 +7,12 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(
+      aPerformance,
+      playFor(aPerformance) //-> 공연 정보를 계산기로 전달
+    );
     const result = Object.assign({}, aPerformance);
-    result.play = playFor(result);
+    result.play = calculator.play;
     result.amount = amountFor(result);
     result.volumeCredits = volumeCreditsFor(result);
     return result;
@@ -60,9 +63,9 @@ export default function createStatementData(invoice, plays) {
   }
 }
 
-//-> 공연료 계산기 클래스
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
