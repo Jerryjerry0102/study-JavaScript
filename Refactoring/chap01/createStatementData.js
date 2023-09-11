5b83da44218b48d836a8711787e80d1b8b711a25
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 20:59:23 +0900

    1.8 공연료 계산기를 다형성 버전으로 만들기: 생성자를 팩터리 함수로 바꾸기

--- Refactoring/chap01/createStatementData.js
+++ Refactoring/chap01/createStatementData.js
@@ -7,7 +7,8 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(
+    const calculator = createPerformanceCalculator(
+      //-> 생성자 대신 팩터리 함수 이용
       aPerformance,
       playFor(aPerformance)
     );
@@ -39,6 +40,10 @@ export default function createStatementData(invoice, plays) {
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 class PerformanceCalculator {
