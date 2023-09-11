507921126c62f148f1fea04064bc0f7752bf524e
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 17:44:08 +0900

    1.6 각 코드를 별도 파일에 저장(statement.js, createStatementData.js) & 반환 값을 저장할 변수의 이름 result로 변경

--- Refactoring/chap01/createStatementData.js
+++ Refactoring/chap01/createStatementData.js
@@ -1,18 +1,10 @@
-import invoices from "./invoices.json" assert { type: "json" };
-import plays from "./plays.json" assert { type: "json" };
-
-function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
-}
-
-//-> 중간 데이터 생성을 전담
-function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
+export default function createStatementData(invoice, plays) {
+  const result = {};
+  result.customer = invoice.customer;
+  result.performances = invoice.performances.map(enrichPerformance);
+  result.totalAmount = totalAmount(result);
+  result.totalVolumeCredits = totalVolumeCredits(result);
+  return result;
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -66,25 +58,3 @@ function createStatementData(invoice, plays) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
-
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-  for (let perf of data.performances) {
-    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
-  return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
-}
-
-console.log(statement(invoices[0], plays));
