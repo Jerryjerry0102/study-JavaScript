66fc4f04cdbfa4a2432e36b7254dee8ee61f32c6
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-11 17:10:29 +0900

    1.6 함수 옮기기 & 중간 데이터에 연극 정보를 저장 & 중간 데이터 사용

--- Refactoring/chap01/temp.js
+++ Refactoring/chap01/temp.js
@@ -8,8 +8,14 @@ function statement(invoice, plays) {
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance); // 얕은 복사 수행
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result); //-> 중간 데이터에 연극 정보를 저장
     return result;
+  }
+
+  //-> renderPlainText()의 중첩 함수였던 playFor()을 statement()로 옮김
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
   }
 }
 
@@ -17,7 +23,7 @@ function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명: ${data.customer})\n`;
 
   for (let perf of data.performances) {
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
+    result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
   }
@@ -53,19 +59,15 @@ function renderPlainText(data, plays) {
   function volumeCreditsFor(aPerformance) {
     let result = 0;
     result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === playFor(aPerformance).type)
+    if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
     return result;
-  }
-
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
   }
 
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy":
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -80,7 +82,7 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르": ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르": ${aPerformance.play.type}`);
     }
 
     return result;
