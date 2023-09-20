ff21c6fb91fe5f68ec933393c082bec9210b4fee
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-20 15:40:29 +0900

    4.6 경계 조건 검사하기

--- Refactoring/chap04/Province.test.js
+++ Refactoring/chap04/Province.test.js
@@ -29,4 +29,51 @@ describe("province", () => {
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
   });
+  test("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+  test("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+  test("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shortfall).toBeNaN();
+    expect(asia.profit).toBeNaN();
+  });
+});
+
+describe("no producers", () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  test("shortfall", () => {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  test("profit", () => {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+
+describe("string for producers", () => {
+  test("", () => {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).toBe(0);
+  });
 });
