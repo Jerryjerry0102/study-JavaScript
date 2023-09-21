bff39994bf0e7aa872db6ef6294b46a31447cc7c
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-21 17:01:41 +0900

    6.3 변수 추출하기

---
+++ Refactoring/chap06/6.3.ExtractVariable.js
@@ -0,0 +1,34 @@
+function price(order) {
+  const basePrice = order.quantity * order.itemPrice;
+  const quantityDiscount =
+    Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
+  const shipping = Math.min(basePrice * 0.1, 100);
+  return basePrice - quantityDiscount + shipping;
+}
+
+class Order {
+  #data;
+  constructor(aRecord) {
+    this.#data = aRecord;
+  }
+
+  get quantity() {
+    return this.#data.quantity;
+  }
+  get itemPrice() {
+    return this.#data.itemPrice;
+  }
+
+  get Price() {
+    return this.basePrice - this.quantityDiscount + this.shipping;
+  }
+  get basePrice() {
+    return this.quantity * this.itemPrice;
+  }
+  get quantityDiscount() {
+    return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;
+  }
+  get shipping() {
+    return Math.min(this.basePrice * 0.1, 100);
+  }
+}
