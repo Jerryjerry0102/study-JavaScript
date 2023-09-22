97a50c37e6972c46dce1f36d2995e31f2fcb9083
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-22 16:33:52 +0900

    6.11 단계 쪼개기

---
+++ Refactoring/chap06/6.11.SplitPhase.js
@@ -0,0 +1,28 @@
+function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  return applyShipping(priceData, shippingMethod);
+}
+
+// 첫 번째 단계를 처리하는 함수
+function calculatePricingData(product, quantity) {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  return {
+    basePrice: basePrice,
+    quantity: quantity,
+    discount: discount,
+  };
+}
+
+// 두 번째 단계를 처리하는 함수
+function applyShipping(priceData, shippingMethod) {
+  const shippingPerCase =
+    priceData.basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = priceData.quantity * shippingPerCase;
+  return priceData.basePrice - priceData.discount + shippingCost;
+}
