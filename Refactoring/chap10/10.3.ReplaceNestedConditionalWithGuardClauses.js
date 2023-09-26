9b7ee27a0b56078666d6465224ca7cc9e870e719
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-26 22:43:17 +0900

    10.3 중첩 조건문을 보호 구문으로 바꾸기

---
+++ Refactoring/chap10/10.3.ReplaceNestedConditionalWithGuardClauses.js
@@ -0,0 +1,24 @@
+function payAmount(employee) {
+  if (employee.isSeparataed) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  // 급여 계산 로직
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  sed.do.eiusmod = tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(minim.veniam);
+  return someFinalComputation();
+}
+
+// 조건 반대로 만들기
+function adjustedCapital(anInstrument) {
+  if (
+    anInstrument.capital <= 0 ||
+    anInstrument.interestRate <= 0 ||
+    anInstrument.duration <= 0
+  )
+    return 0;
+  return (
+    (anInstrument.income / anInstrument.duration) *
+    anInstrument.adjustmentFactor
+  );
+}
