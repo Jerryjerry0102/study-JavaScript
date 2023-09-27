d4b8dfc5514b9a656e7e0e5998e9e743d283f0a7
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-09-27 15:44:11 +0900

    10.7 제어 플래그를 탈출문으로 바꾸기

---
+++ Refactoring/chap10/10.7.ReplaceControlFlagWithBreak.js
@@ -0,0 +1,20 @@
+// 생략(중요하지 않은 코드)
+checkForMiscreants(people);
+// 생략
+
+function checkForMiscreants(people) {
+  for (const p of people) {
+    if (p === "조거") {
+      sendAlert();
+      return;
+    }
+    if (p === "사루만") {
+      sendAlert();
+      return;
+    }
+  }
+}
+
+function checkForMiscreants(people) {
+  if (people.some((p) => ["조커", "사루만"].includes(p))) sendAlert();
+}
