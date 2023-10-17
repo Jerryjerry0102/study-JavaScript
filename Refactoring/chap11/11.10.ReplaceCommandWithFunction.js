cf853dbae394edb485b9c1ca472d5950ebc69ba4
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-17 16:36:43 +0900

    11.10 명령을 함수로 바꾸기

---
+++ Refactoring/chap11/11.10.ReplaceCommandWithFunction.js
@@ -0,0 +1,7 @@
+function charge(customer, usage, provider) {
+  const baseCharge = customer.baseRate * usage;
+  return baseCharge + provider.connectionCharge;
+}
+
+// 호출자
+monthCharge = charge(customer, usage, provide);
