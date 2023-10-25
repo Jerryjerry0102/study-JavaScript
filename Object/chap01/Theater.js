593cf2a23f9aedf76a7894a89bbc7dd9777fa1ea
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2023-10-25 15:22:39 +0900

    refactor: TicketSeller 캡슐화 개선

--- Object/chap01/Theater.js
+++ Object/chap01/Theater.js
@@ -7,14 +7,6 @@ class Theater {
 
   // 관람객 맞이하기
   enter(audience) {
-    if (audience.bag.hasInviation()) {
-      const ticket = this.#ticketSeller.ticketOffice.ticket;
-      audience.bag.ticket = ticket;
-    } else {
-      const ticket = this.#ticketSeller.ticketOffice.ticket;
-      audience.bag.minusAmount(ticket.fee);
-      this.#ticketSeller.ticketOffice.plusAmount(ticket.fee);
-      audience.bag.ticket = ticket;
-    }
+    this.#ticketSeller.sellTo(audience);
   }
 }
--- Object/chap01/TicketSeller.js
+++ Object/chap01/TicketSeller.js
@@ -5,7 +5,15 @@ class TicketSeller {
     this.#ticketoffice = ticketOffice;
   }
 
-  get ticketOffice() {
-    return this.#ticketoffice;
+  sellTo(audience) {
+    if (audience.bag.hasInviation()) {
+      const ticket = this.#ticketoffice.ticket;
+      audience.bag.ticket = ticket;
+    } else {
+      const ticket = this.#ticketOffice.ticket;
+      audience.bag.minusAmount(ticket.fee);
+      this.#ticketOffice.plusAmount(ticket.fee);
+      audience.bag.ticket = ticket;
+    }
   }
 }
